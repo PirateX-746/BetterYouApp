82781390a4ebce829c97d86d62e5962b532ec698
Keyur Thakkar <keyurthakkar746.kt@gmail.com>
2026-03-02 16:06:08 +0530

    Remove excess spacing on desktop - mt-16 only for mobile

--- frontend/src/app/(patient)/layout.tsx
+++ frontend/src/app/(patient)/layout.tsx
@@ -25,7 +25,7 @@ export default function PatientLayout({
       <PatientSidebar />
 
       {/* Main Content */}
-      <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8 pb-24 md:pb-6 mt-16 md:mt-16">
+      <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8 pb-24 md:pb-6 mt-16 md:mt-0">
         {children}
       </main>
 
